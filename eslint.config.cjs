95421ea7ed78d02efc16b3cbb8d543a12ef54244
Abe <field.abe.d@gmail.com>
2025-09-23 11:39:45 -0600

    attempts to fix formatting

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,10 +1,34 @@
 const js = require('@eslint/js');
+const globals = require('globals');
 
 module.exports = [
   js.configs.recommended,
+
   {
-    languageOptions: { ecmaVersion: 'latest', sourceType: 'commonjs' },
+    files: ['**/*.js'],
+    ignores: ['coverage/**'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'commonjs',
+      globals: {
+        ...globals.node,
+        fetch: 'readonly',
+      },
+    },
     rules: {
-    }
-  }
+    },
+  },
+
+  {
+    files: ['test/**/*', '**/*.test.js'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+        fetch: 'readonly',
+      },
+    },
+    rules: {
+    },
+  },
 ];
